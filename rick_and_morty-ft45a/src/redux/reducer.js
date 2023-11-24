8e7663fd179ed6eb6e177ef1d9d7810a7c055c98
RonalReds <ronalrojas.92@gmail.com>
2023-11-24 09:50:17 -0400

    HW Hooks

---
+++ rick_and_morty-ft45a/src/redux/reducer.js
@@ -0,0 +1,51 @@
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./action-type";
+
+const initialState = {
+    myFavorites: [],
+    allCharacters: []
+}
+
+
+const reducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case ADD_FAV:
+            return {
+                ...state,
+                allCharacters: [...state.allCharacters, payload],
+                myFavorites: [...state.myFavorites, payload]
+            }
+        case REMOVE_FAV:
+            const remoFav = state.allCharacters.filter(fav => fav.id !== Number(payload));
+            return {
+                ...state,
+                allCharacters: remoFav,
+                myFavorites: remoFav
+            }
+        case FILTER:
+            if (payload === 'All') return {
+                ...state,
+                myFavorites: state.allCharacters
+            }
+            
+            return {
+                ...state,
+                myFavorites: state.allCharacters.filter(char => char.gender === payload)
+            }
+        
+        case ORDER:
+            const orderCopy = [...state.myFavorites]
+            if (payload === 'A')
+                orderCopy.sort((a, b) => a.id - b.id)
+            if (payload === 'D')
+            orderCopy.sort((a, b) => b.id - a.id)
+            return {
+                ...state,
+                myFavorites: orderCopy
+            }
+        default:
+            return { ...state };
+    }        
+}
+
+
+export default reducer;
