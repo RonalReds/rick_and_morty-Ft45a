119b63eb0467088f4ad0e53396ecb005d6151336
RonalReds <ronalrojas.92@gmail.com>
2023-11-29 09:49:49 -0400

    HW Server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,25 @@
 const http = require('http');
+const PORT = 3001;
+const characters = require('./utils/data');
 
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    if(req.url === '/rickandmorty/character')
-}).listen(3001, 'localhost')
+
+    if (req.url.includes('/rickandmorty/character')) {
+        const id = req.url.split('/').pop();
+        const character = characters.find(char => char.id === Number(id));
+        if (character) {
+            return res
+                .writeHead(200, { 'Content-Type': 'application/json' })
+                .end(JSON.stringify(character));
+        } else {
+            return res
+                .writeHead(404, { 'Content-Type': 'application/json' })
+                .end(JSON.stringify({message: 'Character Not Found'}));
+        }
+    }
+    return res
+        .writeHead(404, { 'Content-Type': 'application/json' })
+        .end(JSON.stringify({message: 'Wrong url'}));
+}).listen(PORT, '127.0.0.1')
